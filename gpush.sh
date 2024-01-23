a8effa649f14a4e2b4a79287ded834521c3471fa
dmshakur <43429414+dmshakur@users.noreply.github.com>
2024-01-23 11:03:45 -0500

    Update gpush.sh

--- gpush.sh
+++ gpush.sh
@@ -1,4 +1,5 @@
-#!/usr/bin/env bash
+# This file is slightly necessary in my development environment
+# As this program was created in termux on an android phone
 eval "$(ssh-agent -s)"
 ssh-add alo
 git add .
